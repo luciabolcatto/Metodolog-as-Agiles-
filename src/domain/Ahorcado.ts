fb6f3e629a1697e39c3edc491102e27efad155ad
FranciscoVacs <franciscovacs@gmail.com>
2026-06-22 21:50:30 -0300

    GREEN: UT - adivinar una letra correcta la revela en la palabra

--- src/domain/Ahorcado.ts
+++ src/domain/Ahorcado.ts
@@ -1,13 +1,21 @@
 export class Ahorcado {
   private palabra: string;
   private _vidas: number = 6;
+  private letrasAdivinadas: Set<string> = new Set();
 
   constructor(palabra: string) {
     this.palabra = palabra;
   }
 
+  adivinar(letra: string): void {
+    this.letrasAdivinadas.add(letra.toUpperCase());
+  }
+
   palabraEnmascarada(): string {
-    return this.palabra.split("").map(() => "_").join(" ");
+    return this.palabra
+      .split("")
+      .map((l) => (this.letrasAdivinadas.has(l) ? l : "_"))
+      .join(" ");
   }
 
   vidas(): number {
