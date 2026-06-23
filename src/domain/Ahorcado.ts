8b753058332da58dc1924f3071e0e9cd69be5b4f
FranciscoVacs <franciscovacs@gmail.com>
2026-06-22 21:39:41 -0300

    GREEN: UT - la palabra empieza completamente enmascarada

---
+++ src/domain/Ahorcado.ts
@@ -0,0 +1,11 @@
+export class Ahorcado {
+  private palabra: string;
+
+  constructor(palabra: string) {
+    this.palabra = palabra;
+  }
+
+  palabraEnmascarada(): string {
+    return this.palabra.split("").map(() => "_").join(" ");
+  }
+}
