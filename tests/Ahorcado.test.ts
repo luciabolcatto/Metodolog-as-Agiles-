a6595ce48593f1a48035635af077a631093a89a0
FranciscoVacs <franciscovacs@gmail.com>
2026-06-22 21:41:03 -0300

    RED: UT - las vidas iniciales son 6

--- tests/Ahorcado.test.ts
+++ tests/Ahorcado.test.ts
@@ -5,3 +5,8 @@ it("la palabra empieza completamente enmascarada", () => {
   const juego = new Ahorcado("GATO");
   expect(juego.palabraEnmascarada()).toBe("_ _ _ _");
 });
+
+it("las vidas iniciales son 6", () => {
+  const juego = new Ahorcado("GATO");
+  expect(juego.vidas()).toBe(6);
+});
